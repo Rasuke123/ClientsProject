cb79b298caf5f0444cbc2af5a05f7b2fd81171e4
Rafael Henrique <henrique171999@hotmail.com>
2026-05-06 19:06:39 +0100

    Connecting with H2 Database

--- src/main/java/io/github/rasuke123/clientsproject/ClientsProjectApplication.java
+++ src/main/java/io/github/rasuke123/clientsproject/ClientsProjectApplication.java
@@ -7,14 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class ClientsProjectApplication {
-
-    @Autowired
-    @Qualifier("applicationName")
-    private String applicationName;
-
-
 
     public static void main(String[] args) {
 
